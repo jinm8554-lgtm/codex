52919c5b347bfe9d565236ae30b312651babcdda
jinm8554-lgtm <jinm8554@gmail.com>
2026-04-30 19:46:54 +0000

    Resolve merge conflicts across homepage files

--- script.js
+++ script.js
@@ -6,8 +6,10 @@ if (yearText) {
   yearText.textContent = new Date().getFullYear();
 }
 
+const greetingText = "你好，很高兴认识你！欢迎来到我的个人主页。";
+
 if (helloButton && helloMessage) {
   helloButton.addEventListener("click", () => {
-    helloMessage.textContent = "你好，很高兴认识你！欢迎来到我的个人主页。";
+    helloMessage.textContent = greetingText;
   });
 }
